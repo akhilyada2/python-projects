8b5507da36dcc86147de05cd405d724a3a585c4e
akhilyada2 <yadaakhil13@gmail.com>
2024-07-25 16:39:39 -0400

    Update rock_paper_scissor.py

--- rock_paper_scissor.py
+++ rock_paper_scissor.py
@@ -37,11 +37,13 @@ if user_choice >= 3 or user_choice < 0:
     print(f"You typed an invalid number, you lose!")
 elif user_choice == 0 and computer_choice == 2:
     print("You win!")
+elif user_choice == 2 and computer_choice == 0:
+    print("You lose")
 elif user_choice == 1 and computer_choice == 0:
     print("You win!")
+elif user_choice == 1 and computer_choice == 2:
+    print("You lose")
 elif user_choice == 2 and computer_choice == 1:
     print("You win!")
 elif user_choice == computer_choice:
     print("It's a draw")
-else:
-    print("You lose")
